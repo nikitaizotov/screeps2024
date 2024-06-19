6c406fe76a7dfa51b366e75d6032c0272ae0d2c9
Nikita Izotov <nikita.izotov@ridango.com>
2024-06-19 21:16:49 +0300

    update

--- dist/build.service.js
+++ dist/build.service.js
@@ -1,5 +1,18 @@
 module.exports = {
-  buildOrder: [STRUCTURE_EXTENSION, STRUCTURE_TOWER],
+  structureCache: {},
+  cachedPaths: [],
+  exitZones: [],
+  buildOrder: [
+    STRUCTURE_EXTENSION,
+    STRUCTURE_TOWER,
+    STRUCTURE_STORAGE,
+    STRUCTURE_TERMINAL,
+    STRUCTURE_LAB,
+    STRUCTURE_OBSERVER,
+    STRUCTURE_NUKER,
+    STRUCTURE_POWER_SPAWN,
+  ],
+
   run: function () {
     if (Game.time % 15000 === 0) {
       this.planRoads();
@@ -8,25 +21,23 @@ module.exports = {
       this.processBuildOrder();
     }
   },
-  processBuildOrder: function () {
-    const room = Game.rooms[Object.keys(Game.rooms)[0]];
 
-    for (let i = 0; i < this.buildOrder.length; i++) {
-      const structureType = this.buildOrder[i];
-      const availableCount = this.getAvailableStructureCount(
-        room,
-        structureType
-      );
-
-      if (availableCount > 0) {
-        this.buildStructure(structureType, availableCount);
-        break;
-      }
-    }
-  },
   planRoads: function () {
-    let keyPoints = this.getKeyPoints(Game.rooms[Object.keys(Game.rooms)[0]]);
     this.cachedPaths = [];
+
+    let keyPoints = [];
+
+    for (let spawnName in Game.spawns) {
+      keyPoints.push(Game.spawns[spawnName].pos);
+    }
+
+    let sources = Game.rooms[Object.keys(Game.rooms)[0]].find(FIND_SOURCES);
+    for (let source of sources) {
+      keyPoints.push(source.pos);
+    }
+
+    let controller = Game.rooms[Object.keys(Game.rooms)[0]].controller;
+    keyPoints.push(controller.pos);
 
     for (let i = 0; i < keyPoints.length; i++) {
       for (let j = i + 1; j < keyPoints.length; j++) {
@@ -74,77 +85,144 @@ module.exports = {
       }
     }
   },
-  buildStructure: function (type, maxCount) {
-    let room = Game.rooms[Object.keys(Game.rooms)[0]];
+
+  processBuildOrder: function () {
+    const room = Game.rooms[Object.keys(Game.rooms)[0]];
+    this.exitZones = this.getExitZones(room);
+
+    for (let i = 0; i < this.buildOrder.length; i++) {
+      const structureType = this.buildOrder[i];
+      const availableCount = this.getAvailableStructureCount(
+        room,
+        structureType
+      );
+
+      if (availableCount > 0) {
+        this.buildStructure(room, structureType, availableCount);
+        break;
+      }
+    }
+  },
+
+  buildStructure: function (room, type, maxCount) {
+    let structuresPlanned = 0;
     let spawns = room.find(FIND_MY_SPAWNS);
     if (spawns.length === 0) return;
 
     let spawnPos = spawns[0].pos;
-    let structuresPlanned = 0;
 
-    let availableCount = this.getAvailableStructureCount(room, type);
+    for (let radius = 1; structuresPlanned < maxCount; radius += 4) {
+      for (let xOffset = -radius; xOffset <= radius; xOffset += 4) {
+        for (let yOffset = -radius; yOffset <= radius; yOffset += 4) {
+          let x = spawnPos.x + xOffset;
+          let y = spawnPos.y + yOffset;
 
-    let countToBuild = Math.min(maxCount, availableCount);
-    let radius = 1;
-    while (structuresPlanned < countToBuild) {
-      let positions = this.getDoubleCirclePositions(spawnPos, radius);
+          if (this.isRestrictedZone(room, x, y)) {
+            continue;
+          }
 
-      for (let pos of positions) {
-        if (this.isValidConstructionPosition(room, pos.x, pos.y)) {
-          if (room.createConstructionSite(pos.x, pos.y, type) === OK) {
-            structuresPlanned++;
-            if (structuresPlanned >= countToBuild) {
-              return;
-            }
+          let positions = [
+            { x: x, y: y },
+            { x: x + 1, y: y },
+            { x: x, y: y + 1 },
+            { x: x + 1, y: y + 1 },
+          ];
+
+          let canBuildGroup = positions.every((pos) =>
+            this.isValidConstructionPosition(room, pos.x, pos.y)
+          );
+
+          if (canBuildGroup) {
+            positions.forEach((pos) => {
+              if (room.createConstructionSite(pos.x, pos.y, type) === OK) {
+                structuresPlanned++;
+                if (structuresPlanned >= maxCount) {
+                  return;
+                }
+              }
+            });
           }
         }
       }
-
-      radius += 3;
     }
   },
-  getDoubleCirclePositions: function (center, radius) {
-    let positions = [];
-    let innerRadius = radius;
-    let outerRadius = radius + 1;
 
-    // Первый ряд
-    for (let dx = -innerRadius; dx <= innerRadius; dx++) {
-      positions.push({ x: center.x + dx, y: center.y - innerRadius });
-      positions.push({ x: center.x + dx, y: center.y + innerRadius });
-    }
-    for (let dy = -innerRadius + 1; dy <= innerRadius - 1; dy++) {
-      positions.push({ x: center.x - innerRadius, y: center.y + dy });
-      positions.push({ x: center.x + innerRadius, y: center.y + dy });
+  isRestrictedZone: function (room, x, y) {
+    for (let zone of this.exitZones) {
+      if (
+        x >= zone.xMin &&
+        x <= zone.xMax &&
+        y >= zone.yMin &&
+        y <= zone.yMax
+      ) {
+        return true;
+      }
     }
 
-    // Второй ряд
-    for (let dx = -outerRadius; dx <= outerRadius; dx++) {
-      positions.push({ x: center.x + dx, y: center.y - outerRadius });
-      positions.push({ x: center.x + dx, y: center.y + outerRadius });
-    }
-    for (let dy = -outerRadius + 1; dy <= outerRadius - 1; dy++) {
-      positions.push({ x: center.x - outerRadius, y: center.y + dy });
-      positions.push({ x: center.x + outerRadius, y: center.y + dy });
+    for (let path of this.cachedPaths) {
+      for (let pos of path) {
+        if (Math.abs(pos.x - x) <= 1 && Math.abs(pos.y - y) <= 1) {
+          return true;
+        }
+      }
     }
 
-    return positions;
+    return false;
   },
+
   isValidConstructionPosition: function (room, x, y) {
-    if (x < 0 || y < 0 || x >= 49 || y >= 49) return false;
+    if (x <= 0 || y <= 0 || x >= 49 || y >= 49) return false;
     if (room.lookForAt(LOOK_TERRAIN, x, y)[0] === "wall") return false;
     if (room.lookForAt(LOOK_STRUCTURES, x, y).length > 0) return false;
     if (room.lookForAt(LOOK_CONSTRUCTION_SITES, x, y).length > 0) return false;
 
-    let exits = room.find(FIND_EXIT);
-    for (let exit of exits) {
-      if (Math.abs(exit.x - x) <= 5 || Math.abs(exit.y - y) <= 5) {
-        return false;
+    return true;
+  },
+
+  getKeyPoints: function (room) {
+    let keyPoints = [];
+
+    let sources = room.find(FIND_SOURCES);
+    for (let source of sources) {
+      keyPoints.push(source.pos);
+    }
+
+    if (room.controller) {
+      keyPoints.push(room.controller.pos);
+    }
+
+    let spawns = room.find(FIND_MY_SPAWNS);
+    for (let spawn of spawns) {
+      keyPoints.push(spawn.pos);
+    }
+
+    return keyPoints;
+  },
+
+  getExitZones: function (room) {
+    let exitZones = [];
+    const exitTypes = [
+      FIND_EXIT_TOP,
+      FIND_EXIT_RIGHT,
+      FIND_EXIT_BOTTOM,
+      FIND_EXIT_LEFT,
+    ];
+
+    for (const exitType of exitTypes) {
+      const exitPositions = room.find(exitType);
+      for (const pos of exitPositions) {
+        exitZones.push({
+          xMin: Math.max(0, pos.x - 5),
+          xMax: Math.min(49, pos.x + 5),
+          yMin: Math.max(0, pos.y - 5),
+          yMax: Math.min(49, pos.y + 5),
+        });
       }
     }
 
-    return true;
+    return exitZones;
   },
+
   getAvailableStructureCount: function (room, structureType) {
     const controllerLevel = room.controller.level;
     const maxStructures = CONTROLLER_STRUCTURES[structureType][controllerLevel];

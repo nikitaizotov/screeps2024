72a7d1b941b4fe34f66166a8d489d77bb0f949bd
Nikita Izotov <nikita.izotov@ridango.com>
2024-06-25 21:04:12 +0300

    Dist folder removed from repo.

--- dist/conatiner.service.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var containerService = {
-    buildContainers: function (room) {
-        if (room.controller && room.controller.my) {
-        }
-    },
-};
-exports.default = containerService;
--- dist/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var room_service_1 = __importDefault(require("./room.service"));
-module.exports.loop = function () {
-    room_service_1.default.routines();
-};

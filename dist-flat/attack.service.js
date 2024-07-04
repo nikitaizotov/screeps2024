3c36f285bc540f25cc73e5811901bc6fe29c800f
Nikita Izotov <nikita.izotov@ridango.com>
2024-07-04 17:51:53 +0300

    Cleanup.

--- dist-flat/attack.service.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const attackService = {
-    avoidPlayers: ["Aleksandra"],
-};
-exports.default = attackService;
--- dist-flat/role.const.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-const RoleCreepsPerRoom = {
-    Dynamic: "DYNAMIC",
-};
